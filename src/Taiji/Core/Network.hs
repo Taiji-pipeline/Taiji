1a547750bc7d98f91656dabad11b915d6e7129ce
Kai Zhang <kai@kzhang.org>
2018-06-09 10:56:52 -0700

    add p-values

--- src/Taiji/Core/Network.hs
+++ src/Taiji/Core/Network.hs
@@ -13,24 +13,26 @@ import           Bio.Utils.Misc                    (readDouble)
 import           Conduit
 import           Control.Lens                      hiding (pre, to)
 import           Control.Monad
+import           Control.Monad.ST (runST)
 import           Control.Monad.Reader              (asks)
 import qualified Data.ByteString.Char8             as B
 import           Data.CaseInsensitive              (CI, mk, original)
 import           Data.Conduit.Cereal               (conduitGet2)
 import           Data.Double.Conversion.ByteString (toShortest)
 import           Data.Either                       (fromRight)
-import           Data.List                         (transpose)
+import           Data.List                         (transpose, sort)
 import           Data.List.Ordered                 (nubSort)
 import qualified Data.Map.Strict                   as M
 import qualified Data.Matrix.Unboxed               as MU
-import           Data.Maybe                        (fromMaybe, mapMaybe)
+import           Data.Maybe                        (fromMaybe, mapMaybe, fromJust)
 import           Data.Monoid                       ((<>))
 import           Data.Serialize                    (decode, encode, get)
 import qualified Data.Text                         as T
 import qualified Data.Vector.Unboxed               as U
 import           IGraph
-import           IGraph.Algorithms (pagerank)
+import           IGraph.Algorithms (pagerank, rewireEdges)
 import           Scientific.Workflow               hiding (_data)
+import Data.Vector.Algorithms.Search (binarySearch)
 
 import           Taiji.Core.Config                 ()
 import           Taiji.Types
@@ -41,18 +43,17 @@ computeRanks :: ( ATACSeq S (File '[] 'Other)
 computeRanks (atac, expr) = do
     dir <- asks ((<> "/Network") . _taiji_output_dir) >>= getPath
     liftIO $ do
-        network <- mkNetwork $ atac^.replicates._2.files.location
-        gr <- fmap pageRank $ case expr of
-            Nothing -> return network
+        let output = dir ++ "/" ++ T.unpack grp ++ "_network.bin"
+        network <- case expr of
+            Nothing -> mkNetwork input
             Just e -> do
                 expr' <- readExpression 1
                     (B.pack $ T.unpack grp ) $ e^.location
-                return $ assignWeights expr' network
-        let output = dir ++ "/" ++ T.unpack grp ++
-                "_network.bin"
-        B.writeFile output $ encode gr
+                assignWeights expr' <$> mkNetwork input
+        getRankPvalue 10 (pageRank network) >>= B.writeFile output . encode
         return (grp, location .~ output $ emptyFile)
   where
+    input = atac^.replicates._2.files.location
     grp = atac^.groupName._Just
 
 mkNetwork :: FilePath -> IO (Graph 'D NetNode NetEdge)
@@ -61,8 +62,8 @@ mkNetwork input = runResourceT $ fromLabeledEdges' input toEdge
     toEdge fl = sourceFileBS fl .| conduitGet2 get .| concatMapC f
       where
         f (gene, tfs1, tfs2) = zipWith g (repeat gene) $ M.toList $ M.fromListWith (+) $ tfs1 ++ tfs2
-        g a b = ( (NetNode a Nothing Nothing Nothing, NetNode (fst b) Nothing Nothing Nothing)
-                , NetEdge Nothing Nothing (snd b) )
+        g a b = ( (defaultNode { nodeName = a }, defaultNode { nodeName = fst b })
+                , defaultEdge { sites = snd b } )
 {-# INLINE mkNetwork #-}
 
 assignWeights :: M.Map (CI B.ByteString) (Double, Double)   -- ^ Gene expression
@@ -115,12 +116,31 @@ pageRank gr = nmap (\(i, x) -> x{pageRankScore=Just $ ranks U.! i}) gr
         getSiteWeight sites
 {-# INLINE pageRank #-}
 
+-- | Compute p-values for PageRank scores, by randomizing the networks.
+getRankPvalue :: Int   -- ^ The number of randomization to be performed
+              -> Graph 'D NetNode NetEdge
+              -> IO (Graph 'D NetNode NetEdge)
+getRankPvalue n gr = do
+    gr' <- thaw gr
+    scores <- fmap (U.fromList . sort . concat) $ replicateM n $ do
+        rewireEdges gr' 1 False False
+        map (fromJust . pageRankScore . snd) . labNodes . pageRank <$>
+            unsafeFreeze gr' 
+    return $
+        nmap (\(_, x) -> x {pageRankPvalue = getP scores <$> pageRankScore x}) gr
+  where
+    getP vec x = 1 - fromIntegral (bisect vec x) / fromIntegral (U.length vec) 
+    bisect v x = runST $ do
+        v' <- U.unsafeThaw v
+        binarySearch v' x
+{-# INLINE getRankPvalue #-}
 
 outputRanks :: [(T.Text, File '[] 'Other)]
-            -> WorkflowConfig TaijiConfig FilePath
+            -> WorkflowConfig TaijiConfig ()
 outputRanks inputs = do
     dir <- asks _taiji_output_dir >>= getPath
-    let output = dir ++ "/GeneRanks.tsv"
+    let output1 = dir ++ "/GeneRanks.tsv"
+        output2 = dir ++ "/GeneRanks_PValues.tsv"
 
     ranks <- forM inputs $ \(_, fl) -> liftIO $ do
         gr <- fmap (fromRight (error "decode fail") . decode) $
@@ -131,16 +151,19 @@ outputRanks inputs = do
                 else do
                     let n = nodeLab gr i
                     x <- pageRankScore n
-                    return (nodeName n, x)
+                    p <- pageRankPvalue n
+                    return (nodeName n, (x, p))
     let genes = nubSort $ concatMap M.keys ranks
         header = B.pack $ T.unpack $ T.intercalate "\t" $
             "Gene" : fst (unzip inputs)
         ranks' = flip map ranks $ \r -> flip map genes $
-            \g -> M.findWithDefault 0 g r
+            \g -> M.findWithDefault (0,1) g r
 
-    liftIO $ B.writeFile output $ B.unlines $
-        header : zipWith toBS (map original genes) (transpose ranks')
-    return output
+    liftIO $ do
+        B.writeFile output1 $ B.unlines $ header :
+            zipWith toBS (map original genes) (transpose $ (map.map) fst ranks')
+        B.writeFile output2 $ B.unlines $ header :
+            zipWith toBS (map original genes) (transpose $ (map.map) snd ranks')
   where
     toBS nm xs = B.intercalate "\t" $ nm : map toShortest xs
 
